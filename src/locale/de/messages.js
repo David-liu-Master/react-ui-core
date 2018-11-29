b80bda6bb312d6becc22bca7a7b9000aec09dc27
david-liu-master <david.liu707@hotmail.com>
2018-11-29 20:11:15 +0100

    Add messages

--- src/locale/de/messages.js
+++ src/locale/de/messages.js
@@ -11,19 +11,13 @@
     'openpatch.ui-core.assignment': 'Aufgabe',
     'openpatch.ui-core.cancel': 'Abbrechen',
     'openpatch.ui-core.confirm': 'OK',
-    'openpatch.ui-core.email': 'E-Mail',
-    'openpatch.ui-core.login': 'Login',
     'openpatch.ui-core.max': 'Max',
     'openpatch.ui-core.mean': 'Mittelwerk',
     'openpatch.ui-core.median': 'Median',
     'openpatch.ui-core.min': 'Min',
     'openpatch.ui-core.n': 'N',
-    'openpatch.ui-core.password': 'Passwort',
-    'openpatch.ui-core.register': 'Registrieren',
-    'openpatch.ui-core.repeatPassword': 'Passwort Wiederholen',
     'openpatch.ui-core.standardDeviation': 'SD',
     'openpatch.ui-core.unsupported': 'Nicht unterst\xFCtzt',
-    'openpatch.ui-core.username': 'Benutzername',
     'openpatch.ui-core.variance': 'Varianz'
   }
 };
