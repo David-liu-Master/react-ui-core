fbaa5117c79586293bce62976029aa2e24c29be3
david-liu-master <david.liu707@hotmail.com>
2019-01-06 15:03:36 +0100

    Use Reach Router in proxies

--- src/proxies/ReachRouterProxy.js
+++ src/proxies/ReachRouterProxy.js
@@ -7,22 +7,31 @@ import {
   Router
 } from '../router';
 
-export const withReachRouter = (WrappedComponent, path = '/*') => ({
-  url,
-  ...props
-}) => {
-  const source = createMemorySource(url);
-  const history = createHistory(source);
+export const createReachRouterProxy = () => {
+  class ReachRouterProxy extends React.Component {
+    render() {
+      const {
+        nextProxy,
+        fixture: { url = '/' }
+      } = this.props;
+      const { value: NextProxy, next } = nextProxy;
+      const source = createMemorySource(url);
+      const history = createHistory(source);
 
-  return (
-    <LocationProvider history={history}>
-      <Location>
-        {({ location }) => (
-          <Router location={location}>
-            <WrappedComponent {...props} path={path} />
-          </Router>
-        )}
-      </Location>
-    </LocationProvider>
-  );
+      return (
+        <LocationProvider history={history}>
+          <Location>
+            {({ location }) => (
+              <Router location={location}>
+                <NextProxy {...this.props} nextProxy={next()} path="/*" />
+              </Router>
+            )}
+          </Location>
+        </LocationProvider>
+      );
+    }
+  }
+  return ReachRouterProxy;
 };
+
+export default createReachRouterProxy();
