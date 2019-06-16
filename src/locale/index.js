89bada08192711230064e46a7253cce7d1b90753
david-liu-master <david.liu707@hotmail.com>
2019-06-16 12:11:41 +0200

    Remove mergeLocale

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,14 +1,6 @@
 const de = require('./de/messages.js');
 const en = require('./en/messages.js');
 
-export const mergeLocale = (l1, l2) => {
-  const locale = {
-    languageData: Object.assign({}, l1.languageData, l2.languageData),
-    messages: Object.assign({}, l1.messages, l2.messages)
-  };
-  return locale;
-};
-
 export const locale = {
   de,
   en
