67b26348a15e102f22a7c6bcbc13b6652bd1dfff
david-liu-master <david.liu707@hotmail.com>
2019-07-11 10:49:04 +0200

    Better withReduxFormProvider

--- src/storybookProvider/withReduxFormProvider.js
+++ src/storybookProvider/withReduxFormProvider.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { reduxForm, reducer as formReducer } from 'redux-form';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const withReduxForm = initialValues => storyFunc => {
   const reducers = { form: formReducer };
   const reducer = combineReducers(reducers);
-  const store = createStore(reducer);
+  const store = createStore(
+    reducer,
+    undefined,
+    composeEnhancers(applyMiddleware(thunk))
+  );
   const Test = reduxForm({ form: 'withReduxForm', initialValues })(storyFunc);
   return (
     <Provider store={store}>
