26e64fe472f2b4e138743b95df2e555086a5bc86
david-liu-master <david.liu707@hotmail.com>
2019-07-11 21:02:12 +0200

    Fix test

--- tests/ReduxRemoteTable.test.js
+++ tests/ReduxRemoteTable.test.js
@@ -4,7 +4,7 @@ describe('ReduxRemoteTable constructElasticPagingURL', () => {
   it('should construct', () => {
     const constructPagingURL = constructElasticPagingURL('/v1/items');
     const url = constructPagingURL(3, 5, 'asc', 'member');
-    const expectedURL = `/v1/items?query="{"offset":15,"limit":5,"sort":{"member":"asc"}}"`;
+    const expectedURL = `/v1/items?query=%7B%22offset%22%3A15%2C%22limit%22%3A5%2C%22sort%22%3A%7B%22member%22%3A%22asc%22%7D%7D`;
 
     expect(url).toEqual(expectedURL);
   });
