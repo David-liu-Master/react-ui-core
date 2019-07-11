e1a1aea31cd44c8bac7d09cceb22f3b0f2c19f35
david-liu-master <david.liu707@hotmail.com>
2019-07-11 20:05:36 +0200

    Fix elastic paging

--- src/ReduxRemoteTable.js
+++ src/ReduxRemoteTable.js
@@ -28,7 +28,7 @@ export const constructElasticPagingURL = baseURL => (
       [orderBy]: order
     }
   };
-  return `${baseURL}?query=${JSON.stringify(query)}`;
+  return `${baseURL}?query="${JSON.stringify(query)}"`;
 };
 
 class ReduxRemoteTable extends React.Component {
--- tests/ReduxRemoteTable.test.js
+++ tests/ReduxRemoteTable.test.js
@@ -4,8 +4,7 @@ describe('ReduxRemoteTable constructElasticPagingURL', () => {
   it('should construct', () => {
     const constructPagingURL = constructElasticPagingURL('/v1/items');
     const url = constructPagingURL(3, 5, 'asc', 'member');
-    const expectedURL =
-      '/v1/items?query={"offset":15,"limit":5,"sort":{"member":"asc"}}';
+    const expectedURL = `/v1/items?query="{"offset":15,"limit":5,"sort":{"member":"asc"}}"`;
 
     expect(url).toEqual(expectedURL);
   });
